a4b01f5742e8d58a4370f582c6356e75184c6f91
PechurinaLena <www.waaay.net@gmail.com>
2022-08-11 11:33:05 +0300

    Added actions to load data

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,7 +1,15 @@
 import {createAction} from '@reduxjs/toolkit';
 import {City, Offer} from 'types/offers';
+import {AppRoute, AuthorizationStatus} from 'types/const';
 
-export const setSelectedPoint = createAction('set selected offer', (value: Offer['id']) => ({payload: value}));
-export const setActiveCity = createAction('main/set active city', (value: City) => ({payload: value}));
-export const setSortingOffers = createAction('offers/set sorting offers', (value: string) => ({payload: value}));
-export const setOptionsShown = createAction('offers/set options shown', (value: boolean) => ({payload: value}));
+export const setSelectedPoint = createAction('offers/setSelectedOffers', (value: Offer['id']) => ({payload: value}));
+export const setActiveCity = createAction('offers/setActiveCity', (value: City) => ({payload: value}));
+export const setSortingOffers = createAction('offers/setSortingOffers', (value: string) => ({payload: value}));
+export const setOptionsShown = createAction('offers/setSortOptionsShown', (value: boolean) => ({payload: value}));
+
+export const loadOffers = createAction<Offer[]>('data/loadOffers');
+export const loadFavoritesOffers = createAction<Offer[]>('data/loadFavoritesOffers');
+
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
+export const setDataLoadedStatus = createAction<boolean>('data/setDataLoadedStatus');
+export const redirectToRoute = createAction<AppRoute>('offers/redirectToRoute');
