b1018738e7c8ffeea0f8e13dfc909ce570219fce
PechurinaLena <www.waaay.net@gmail.com>
2022-08-20 16:03:47 +0300

    Fixed link logo in header

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -32,7 +32,7 @@ export const Header: FC<HeaderProps> = ({isLoginPage}) => {
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <Link className="header__logo-link header__logo-link--active" to="/">
+            <Link className="header__logo-link header__logo-link--active" to={AppRoute.Root}>
               <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
             </Link>
           </div>
